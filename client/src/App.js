df9ca850b95b07977cd87e35e0c39ca816c3b9d1
Younis1-png <113353466+Younis1-png@users.noreply.github.com>
2023-01-04 16:18:07 -0500

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const App = () => {
+
+  const router = createBrowserRouter([
+
+  ])
+  
+  return <div>App</div>;
+};
+
+export default App;
