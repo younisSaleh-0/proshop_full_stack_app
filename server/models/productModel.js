ba0a82f283568e9db2881004e50a7d668f8b20f0
Younis1-png <113353466+Younis1-png@users.noreply.github.com>
2023-01-04 23:33:39 -0500

    created userModel, productModel and orderModel

---
+++ server/models/productModel.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+// Review Schema
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    rating: { type: Number, require: true },
+    comment: { type: String, require: true },
+  },
+  { timestamp: true }
+);
+
+// Products schema
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.types.ObjectId,
+      required: true,
+      ref: "User", // Reference model is the User
+      // This help us to identify which user
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+
+    reviews: [reviewSchema], // This reviews is refer to our schema above
+
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamp: true }
+);
+
+//Export the model
+const Product = mongoose.model("Product", productSchema);
+export default Product;
---
+++ server/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+const User = mongoose.model("User", userSchema);
+export default User;
